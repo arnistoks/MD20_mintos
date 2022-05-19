832c6bf8e37d0443e2c231cb1224cf9e6bb0dd9d
arnistoks <arnis.toks@gmail.com>
2022-05-19 13:54:14 +0300

    feat: add boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+type Currency = {
+  id: number;
+  name: string;
+  isActive: boolean;
+}
+
+const currencies: Currency[] = [
+  {
+    id: 1,
+    name: 'EUR',
+    isActive: false,
+  },
+  {
+    id: 2,
+    name: 'PLN',
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: 'GEL',
+    isActive: false,
+  },
+  {
+    id: 4,
+    name: 'DKK',
+    isActive: false,
+  },
+  {
+    id: 5,
+    name: 'CZK',
+    isActive: false,
+  },
+  {
+    id: 6,
+    name: 'GBP',
+    isActive: false,
+  },
+  {
+    id: 7,
+    name: 'SEK',
+    isActive: false,
+  },
+  {
+    id: 8,
+    name: 'USD',
+    isActive: false,
+  },
+  {
+    id: 9,
+    name: 'RUB',
+    isActive: false,
+  },
+];
+
+const App = () => {
+  const [selectedCurrencies, setSelectedCurrencies] = useState<Currency[]>(currencies);
+
+  const activeCurrencies: Currency[] = selectedCurrencies.map((currency) => ({ ...currency, isActive: true } && currency));
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="first__row">
+          {activeCurrencies.map((currency) => (
+            <div
+              key={currency.id}
+              className={currency.isActive ? 'note' : 'hidden'}
+            >
+              {currency?.name.toLowerCase()}
+              <div className="border">
+                <button
+                  className="remove__button"
+                  onClick={() => setSelectedCurrencies(selectedCurrencies.map((curr) => (curr.id === currency.id ? { ...curr, isActive: false } : curr)))}
+                >
+                  X
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="second__row">
+          {selectedCurrencies.map(({ id, name, isActive }) => (
+            <button
+              key={id}
+              className={!isActive ? 'add__button' : 'isActive'}
+              onClick={() => setSelectedCurrencies(selectedCurrencies.map((currency) => (currency.id === id ? { ...currency, isActive: !isActive } : currency)))}
+            >
+              <div className="checkbox">{!isActive ? '' : 'X'}</div>
+              {name}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
